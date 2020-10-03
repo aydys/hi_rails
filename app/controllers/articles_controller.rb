608f7e06770b9285372974042185f356470804b9
aidys23 <aidongak23@gmail.com>
2020-10-03 23:37:59 +0700

    implement article create

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,20 @@
 class ArticlesController < ApplicationController
-    def new
-        
-    end
+  def new
+      
+  end
 
-    def create
-        render plain: params[:article].inspect
+  def create
+    @articles = Article.new(article_params)
+    if @articles.valid?
+      @articles.save
+    else
+      render action: 'new'
     end
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :text)
+  end
 end
