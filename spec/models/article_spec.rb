d40fa3945a63c70df0ba2dbda9919855e3342a35
aidys23 <aidongak23@gmail.com>
2020-10-14 15:36:13 +0700

    update article spec

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -5,7 +5,19 @@ describe Article do
     it { should validate_presence_of :title }
     it { should validate_presence_of :text }  
   end
+
   describe 'associations' do
     it { should have_many :comments }
   end
+
+  describe '#subject' do
+    it 'returns the article title' do
+      # arrange, act
+      article = create(:article, title: 'Lorem Ipsum')
+
+      # assert
+      expect(article.subject).to eq 'Lorem Ipsum'
+    end
+  end
+
 end
