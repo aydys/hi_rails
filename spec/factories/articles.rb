d73339ef14f978a8fa10dd0eef6b663e069c39a3
aidys23 <aidongak23@gmail.com>
2020-10-14 16:07:32 +0700

    update article factory

--- spec/factories/articles.rb
+++ spec/factories/articles.rb
@@ -2,5 +2,15 @@ FactoryBot.define do
   factory :article do
     title { 'Article title' }
     text { 'Article text' }
+
+    # создаем фабрику для создание статьи с нексолькими комментариями
+    factory :article_with_comments do
+      # после создание article
+      after :create do |article, evalutor|
+        # создаем список из 3-х комментариях
+        create_list :comment, 3, article: article
+      end
+    end
+
   end
 end
